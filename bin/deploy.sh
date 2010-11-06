33e01f0ab51cd3b52607fb1742f7140c55d8a89b
Daniel Nephin <daniel@perzoot.com>
2010-11-06 20:53:01 +0000

    Adding staging configuration and deploy script, and moved media into project root.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,53 @@
+#
+# Deployment Script for Perzoot website_main
+#
+# Usage:
+#	./deploy <checkout> <env>
+#	
+#	checkout:	path to the root of the jobsite_web checkout
+#	env:		stage | prod
+#
 
-# added to /etc/apache2/httpd.conf
-# WSGIScriptAlias / /var/perzoot/django.wsgi
+HTTPD_SITES=/etc/apache2/sites-enabled/
 
 
+if [ ! $1 ]; then
+	echo "No checkout dir specified. See usage."
+	exit -1
+fi
 
+if [ ! $2 ]; then
+	echo "No environment specified. See usage. "
+	exit -1
+fi
+
+case $2 in
+	"stage")
+		DEPLOY_ENV=/var/pz_stage
+		CONF_DIR=$1/conf/stage
+		;;
+	"prod")
+		DEPLOY_ENV=/var/perzoot
+		CONF_DIR=$1/conf/prod
+		;;
+	*)
+		echo "Unknown environment $1. Should be stage or prod."
+		exit -2
+		;;
+esac
+
+
+# Copy apache config into sites
+cp $CONF_DIR/$1.conf $HTTPD_SITES
+cp $CONF_DIR/django.wsgi $HTTPD_SITES
+
+# Copy code
+rsync --delete --exclude=.git --exclude=*.pyc --exclude=*.swp $1/jobsite_web $DEPLOY_ENV/jobsite_web
+# Copy media
+rsync --delete --exclude=.git --exclude=*.pyc --exclude=*.swp $1/ $DEPLOY_ENV/jobsite_web
+
+# Copy settings and url
+cp $CONF_DIR/settings.py $DEPLOY_ENV/jobsite_web/
 
 # restart apache2
 sudo service apache2 restart
---
+++ conf/stage/settings.py
@@ -0,0 +1,99 @@
+"""
+ Perzoot - settings.py
+ Staging Environment.
+"""
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+import os
+PROJECT_ROOT = '/var/pz_stage/jobsite_web/'
+
+
+# Solr
+SOLR_URL = 'http://localhost:8080/solr'
+
+
+ADMINS = (
+	('Perzoot Admin', 'admin@perzoot.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'jobs',
+		'USER': 'jobs',
+		'PASSWORD': 'jobspass',
+		'HOST': '',
+		'PORT': '',
+	}
+}
+
+
+# OAuth
+OAUTH_ACCESS_SETTINGS = {
+	'linkedin': {
+		'keys': {
+			'KEY':	  'OVWYIVnMYcNsaHsf3BRhzLMuaviknWiufpDrvJq_Cv5013547ezQioYZpZtZWeE8',
+			'SECRET': 'Jk1yH4a4SvLysl3Ph__E9LIXp_lJgJ_M1DbXU3rj6oyV3ZJUaJqvcAvWu46h-y2H',
+		},
+		'endpoints': {
+			'request_token': 'https://api.linkedin.com/uas/oauth/requestToken',
+			'access_token': 'https://api.linkedin.com/uas/oauth/accessToken',
+			'authorize': 'https://api.linkedin.com/uas/oauth/authorize',
+			'provider_scope':'',
+			'callback': 'jobsite_web.jobsite_main.oauth_callbacks.linked_in',
+		},
+	},
+}
+
+
+# Locale
+TIME_ZONE = 'America/Montreal'
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+
+MEDIA_ROOT = PROJECT_ROOT + '/media/'
+MEDIA_URL = '/m/'
+ADMIN_MEDIA_PREFIX = '/media_admin/'
+SECRET_KEY = '129e12jnakfm23rjf90JIKN@uf4niq2n3fk129jfkn9jf9jrf2300;.'
+ROOT_URLCONF = 'jobsite_web.urls'
+SESSION_COOKIE_AGE = 6 * 7 * 24 * 60 * 60 	# 6 weeks
+
+TEMPLATE_LOADERS = (
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
+#	 'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+	'django.middleware.common.CommonMiddleware',
+#	'django.middleware.cache.UpdateCacheMiddleware',
+
+	# Adds to Vary header
+#	'django.middleware.gzip.GZipMiddleware',
+#	'django.middleware.http.ConditionalGetMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+
+#	'django.middleware.cache.FetchFromCacheMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+TEMPLATE_DIRS = (
+	PROJECT_ROOT + '/templates',
+)
+
+INSTALLED_APPS = (
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.sites',
+	'django.contrib.admin',
+	'jobsite_web.jobsite_main',
+	'oauth_access',
+)
+
