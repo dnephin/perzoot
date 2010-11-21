247b9830c8c51db0a0a6d2398d89d0bae37f5dbf
Daniel Nephin <dnephin@gmail.com>
2010-11-20 20:19:15 -0500

    Adding search history widget on the search page Adding search history retreival Modified SearchEvent model See #5

---
+++ jobsite_web/templates/js_statics.js
@@ -0,0 +1,14 @@
+{% comment %}
+
+ js_statics.js
+
+ This file is used to create javascript global variables for urls.  Since urls
+ should only be hardcoded in urls.py, and javascript files are not served
+ through django, this is a way to access those urls without hardcoding them
+ in javascript files.
+
+
+{% endcomment %}
+var URL_SEARCH = "{% url jobsite_main.views.search %}";
+var URL_SEARCH_HISTORY = "{% url search_history %}";
+var URL_SEARCH_SAVED = "{% url saved_searches %}";
